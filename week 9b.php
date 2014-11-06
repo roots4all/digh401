02c314c4fc47099319025fafc02c681b7ac50cd3
roots4all <roots4all@gmail.com>
2014-11-06 01:01:18 -0600

    Week 9 Assignment
    Week 9 assignment attempt for His 401

---
+++ week 9b.php
@@ -0,0 +1,29 @@
+<?php
+
+$a = sqrt(12);
+$b = 8;
+$c = 2;
+$d = sqrt(15);
+$e = 9;
+$f = 4;
+
+ $x=$a+=$b/=$c;
+		var_dump($x);
+echo "<br>";
+ $y = $d - 9 * $f;
+		var_dump($y);
+echo "<br>";
+	$g = ceil($x);
+		var_dump($g);
+echo "<br>";
+	$h = ceil($y);
+		var_dump($h);
+echo "<br>";
+	$i = max($g,$h);
+
+echo "$i";
+?>
+
+
+
+
